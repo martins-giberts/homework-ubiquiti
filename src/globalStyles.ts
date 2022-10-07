3a30946aa85b99b201a41577d92940cea9634e10
Martins Giberts <martins.giberts@gmail.com>
2022-10-07 16:28:49 +0300

    add tokens

--- src/globalStyles.ts
+++ src/globalStyles.ts
@@ -4,9 +4,10 @@ import { tokens } from "./tokens";
 export const GlobalStyle = createGlobalStyle`
   @import url(tokens.fontLibraryImportUrlLato);
 
-  body {
+  body, html {
     font-family: ${tokens.fontFamily};
     font-size: ${tokens.defaultFontSizePx};
+    height: 100%;
   }
 
   * {
